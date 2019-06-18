8360f0c4b09ab88c90638f9a7805d15b44fbe3c2
Bambooh <cdgaviria40@misena.edu.co>
2019-06-18 10:07:16 -0500

    Modelos de usuarios

--- src/usuarios/admin.py
+++ src/usuarios/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Usuario, TipoUsuario, DatosUsu
+from .models import Usuario, TipoUsuario, DatosUsuario, Titulo, TipoDocumento
 
 admin.site.register(Usuario)
 admin.site.register(TipoUsuario)
-admin.site.register(DatosUsu)
+admin.site.register(DatosUsuario)
+admin.site.register(Titulo)
+admin.site.register(TipoDocumento)
