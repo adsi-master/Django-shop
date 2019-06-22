61ed2d763265cfaef427a5723410d5a0c9f92a78
Hanna <hannamaria446@gmail.com>
2019-06-21 23:50:27 -0500

    modelos producto y lenguaje; actualización BD

--- src/productos/models.py
+++ src/productos/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    lenguaje = models.ForeignKey(Lenguaje, on_delete = models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE) 
+    nombres = models.CharField(max_length = 45, verbose_name = "Nombre")
+    fecha = models.DateTimeField(auto_now_add = True, verbose_name = "Fecha")
+    desProducto = models.CharField(max_length = 80, verbose_name = "Descripción")
+    imagen = models.CharField(max_length = 50, verbose_name = "Imagen")
+    estado = models.CharField(max_length = 50, verbose_name = "Estado" )
+    crated_at = models.DateTimeField(auto_now_add = True, verbose_name = "Registrado el : ")
+    updated_at = models.DateTimeField(auto_now_add = True, verbose_name = "Actualizado el : ")
+
+  class meta : 
+        verbose_name = "producto"
+
+        verbose_name_plural = "productos"
+
+    def __str__ (self):
+        
+        return self.nombres
+
+class Lenguaje(models.Model):
+    nomLenguaje = CharField(max_length = 45)
+    version = CharField(max_length = 45)
+    crated_at = models.DateTimeField(auto_now_add = True, verbose_name = "Registrado el : ")
+    updated_at = models.DateTimeField(auto_now_add = True, verbose_name = "Actualizado el : ")
+
+    class meta:
+        verbose_name = "lenguaje"
+
+        verbose_name_plural = "lenguajes"
+
+    def __str__(self):
+
+        return self.nomLenguaje    
--- tienda/settings.py
+++ tienda/settings.py
@@ -75,14 +75,15 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'tienda',
+        'LOCALHOST':'27017',
+        'USER': 'hanna',
+        'PASSWORD':'1234',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
