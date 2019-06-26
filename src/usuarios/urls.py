0ad573ffec0afb789a9ad8d2908317abe9062fd7
Cesar Berrio <clberrio4@misena.edu.co>
2019-06-26 14:11:02 -0500

    Para no mamarselo a lara login hecho

--- src/usuarios/urls.py
+++ src/usuarios/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import url
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('login/',LoginView.as_view(template_name='usuario/login.html'), name= 'pablo-chupa' ),
+    
 ]
