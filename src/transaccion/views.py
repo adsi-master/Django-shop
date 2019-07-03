5735ce59fd818ec279a9e3e4748b8523f23964ea
mate458 <matepabon@gmail.com>
2019-07-02 22:24:02 -0500

    Se implemento la vista listar de la factura

--- src/transaccion/views.py
+++ src/transaccion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import Transaccion
 # Create your views here.
 
 # class TransaccionList(ListView):
@@ -7,5 +8,9 @@ from django.views.generic import ListView
 def checking(request):
     return render(request, 'dailyShop/checkout.html')
 
-def factura(request):
-    return render(request, 'dailyShop/factura.html')
+class facturaListView(ListView):
+    model = Transaccion
+    template_name = 'dailyShop/factura.html'
+    context_object_name = 'facturaList'
+    def get_queryset():
+        return Transaccion.objects.filter(Usuarioid=1)
